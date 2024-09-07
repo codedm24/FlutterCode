5113f62f4642c09332bbfe18c4fbd7efdadad2ba
codedm24 <mithun.adroit@gmail.com>
2024-09-07 22:36:20 +0530

    Flutter Bottom App Bar
    Flutter mobile app for BottomAppBar

---
+++ ch8_bottom_app_bar/lib/pages/home.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BottomAppBar'),
+      ),
+      body: SafeArea(
+        child: Container(),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.blue.shade200,
+        shape: const CircularNotchedRectangle(),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget> [
+            IconButton(
+              icon: const Icon(Icons.access_alarm),
+              color: Colors.white,
+              onPressed: (){
+                print('alarm icon pressed');
+              },
+            ),
+            IconButton(
+                onPressed: (){
+                  print('bookmark icon pressed');
+                },
+                color: Colors.white,
+                icon: const Icon(Icons.bookmark_border)
+            ),
+            IconButton(
+              onPressed: (){
+                print('flight icon pressed');
+              },
+              icon: const Icon(Icons.flight),
+              color: Colors.white,
+            ),
+            const Divider(),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blue.shade200,
+        onPressed: (){
+          print('add icon pressed');
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
